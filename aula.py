456815b03715fb14b2d250873bd2e56026a09163
DenisCastro1 <49196402+DenisCastro1@users.noreply.github.com>
2019-04-02 10:03:07 -0300

    pesadelomatematico
    problema pesadelo matematico lic

---
+++ aula.py
@@ -0,0 +1,30 @@
+valor=int(input("Digite o valor:"))
+dia=0
+n=valor
+
+while valor>0 :
+    
+    
+
+
+    if valor%2!=0:
+        
+        valor=(valor*3+1)/2
+        
+        dia=dia+1
+       
+    
+        
+    
+        
+        
+    if valor%2==0:
+       
+        valor=valor/2
+        
+        dia=dia+1
+
+    if valor==1:
+        print("Levam",dia,"dias para o preço chegar a 1 real")
+        break
+    
